a7625752a36e12b5594e066540fc7daa3a3c35ae
William Chong <williamchong007@msn.com>
2018-01-16 13:48:54 +0800

    - Do not errorLog code 202 since it is a spam fest

--- ios/validate7_v2.js
+++ ios/validate7_v2.js
@@ -1,5 +1,5 @@
 const validate = require('./validate7.js');
-const { errLog, log } = require('../log.js');
+const { errLog } = require('../log.js');
 const express = require('express');
 
 const router = express.Router();
@@ -9,14 +9,17 @@ router.post('/:bundle/:product_id', (req, res) => {
   if (undefined !== req.query.get_latest_receipt && req.query.get_latest_receipt !== 'false') {
     opt.get_latest_receipt = true;
   }
+
+  /* This info is logged in nginx anway */
+  // log(`ios7_v2: ${req.params.bundle} ${req.params.product_id}`);
+
   // Set up response.
-  log(`ios7_v2: ${req.params.bundle} ${req.params.product_id}`);
   res.writeHead(200, { 'Content-Type': 'application/json' });
 
   // Validate.
   validate(req.params.bundle, req.body.receipt, req.params.product_id, (result) => {
     // Write response.
-    if (result.code !== 0) {
+    if (result.code !== 0 && result.code !== 202) {
       errLog(JSON.stringify({
         type: 'ios7_v2',
         error: result.error,
